acc6bb2dd1d4aa532f1a9e780e18d7a468e0ad20
ming <ming>
2017-08-14 09:27:53 -0700

    update h2 configuration

--- src/main/java/sample/RestApplication.java
+++ src/main/java/sample/RestApplication.java
@@ -19,17 +19,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * @author Rob Winch
  */
 @SpringBootApplication
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
 public class RestApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(RestApplication.class);
